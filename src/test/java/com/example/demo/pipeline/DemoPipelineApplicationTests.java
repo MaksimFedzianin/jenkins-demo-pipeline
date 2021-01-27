1482b967a0ed180142d682c6676d19613e5997fb
Maksim Fedzianin <maksim.fedzianin@gmail.com>
2021-01-27 21:38:53 +0300

    Added tests

--- src/test/java/com/example/demo/pipeline/DemoPipelineApplicationTests.java
+++ src/test/java/com/example/demo/pipeline/DemoPipelineApplicationTests.java
@@ -3,11 +3,24 @@ package com.example.demo.pipeline;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoPipelineApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void dumbTest() {
+		assertTrue(true);
+    }
+
+    @Test
+    void oneMoreDumbTest(){
+    	assertEquals(2, 1 + 1);
 	}
 
 }
